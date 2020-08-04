825e1793dc3daebe0681cb1e3096634da828636d
haikal <haikalharin1998@gmail.com>
2020-08-04 15:49:35 +0700

    Initial commit

---
+++ app/src/main/java/com/haikalharin/loginregister/SuccesActivity.java
@@ -0,0 +1,14 @@
+package com.haikalharin.loginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes);
+    }
+}
